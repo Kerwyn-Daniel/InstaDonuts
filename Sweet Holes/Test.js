92d690d84e38921ae229b16f4c15c8f0346f5be1
Kerwyn Daniel <134152262+Kerwyn-Daniel@users.noreply.github.com>
2023-06-17 00:04:17 -0400

    Add files via upload

---
+++ Sweet Holes/Test.js
@@ -0,0 +1,56 @@
+// //Lets create lifetime counter and current balance as const
+// const lifeTimeCount = 0;
+// const curBal =0;
+
+// //create simple add function line that +1 to balances
+// function addDonut(){
+//     lifeTimeCount += 1;
+//     curBal +=1;
+//     document.getElementById("curCount").style.display="curBal";
+//     document.getElementById("totCount").style.display="lifeTimeCount";
+// }
+
+// //lets create the first main click function and add to both balances
+// // function donutclick(){
+//     var doClick= document.getElementById('adocli');
+//     doClick.onclick = addDonut();  
+
+    // //function to gray out buttons
+// var nuClick = document.getElementById('donutClick');
+// nuClick.onclick = function darkClick(){
+//     if(curBal<oven){
+//         //change button background
+//         document.querySelector("buttona").style.backgroundColor="slate";
+//     }else {
+//         //change background
+//         document.getElementById("easy").style.backgroundColor="white";
+//     }
+//     if(curBal<homemade){
+//         //change button background
+//         document.getElementById("home").style.backgroundColor="slate";
+//     }else {
+//         //change background
+//         document.getElementById("home").style.backgroundColor="white";
+//     }
+//     if(curBal<influence){
+//         //change button background
+//         document.getElementById("tuber").style.backgroundColor="slate";
+//     }else {
+//         //change background
+//         document.getElementById("tube").style.backgroundColor="white";
+//     }
+//     if(curBal<store){
+//         //change button background
+//         document.getElementById("comm").style.backgroundColor="slate";
+//     }else {
+//         //change background
+//         document.getElementById("comm").style.backgroundColor="white";
+//     }
+//     if(curBal<legend){
+//         //change button background
+//         document.getElementById("fred").style.backgroundColor="slate";
+//     }else {
+//         //change background
+//         document.getElementById("fred").style.backgroundColor="white";
+//     }
+// }
